0066cd45979a1cb7841e465e6e9336d9948f6c49
Zaki ul Hassan <your@email.com>
2026-07-20 17:47:07 +0500

    aaahh find nemoooo

---
+++ nemo.js
@@ -0,0 +1,13 @@
+const nemo = ['nemo'];
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('FounD NEMO!');
+    }
+  }
+  let t1 = performance.now();
+  console.log('Call to find NEMO took ' + (t1 - t0) + 'milliseconds');
+}
+
+findNemo(nemo);
