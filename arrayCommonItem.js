e7544d72c3f7e37eba21efc81a5262f06a43e172
Zaki ul Hassan <your@email.com>
2026-08-01 07:36:04 +0500

    Update arrayCommonItem solution

--- arrayCommonItem.js
+++ arrayCommonItem.js
@@ -1,16 +1,55 @@
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['a', 'y', 'z'];
+
+// function containCommonItem (arr1, arr2) {
+//   for(let i = 0; i < arr1.length; i++) {
+//     for(let j = 0; j < arr2.length; j++) {
+//       if(arr1[i] === arr2[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// // Big 0(n^2)
+// console.log(containCommonItem(array1, array2))
+
+
+
+
+//array ==> obj {
+// a:true,
+// b: true,
+// c: true,
+// x: true
+// }
+
+//array2[index] === obj.properties
 const array1 = ['a', 'b', 'c', 'x'];
 const array2 = ['a', 'y', 'z'];
 
-function containCommonItem (arr1, arr2) {
-  for(let i = 0; i < arr1.length; i++) {
-    for(let j = 0; j < arr2.length; j++) {
-      if(arr1[i] === arr2[j]) {
-        return true;
-      }
+
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create object where properties === items in the array
+  // can we assume always 3 params?
+  let map = {}
+  for (let i =0; i < arr1.length; i++) {
+    if(!map[arr1[i]]) {
+      const item = arr1[i]
+      map[item] = true;
     }
   }
-  return false;
-}
+  // loop through second array and check if item in
+  //  second array exists on created object.
+  for(let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
+    }
+  }
+  return false
+ }
 
-// Big 0(n^2)
-console.log(containCommonItem(array1, array2))
+ //O(a + b) Time Complexity
+
+ console.log(containsCommonItem2(array1, array2))
