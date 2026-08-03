5f7e3aec93a7b0b350733da34bc1c0269a456dc4
Zaki ul Hassan <your@email.com>
2026-08-03 10:42:12 +0500

    Space complexity practice examples

---
+++ space_complexity.js
@@ -0,0 +1,25 @@
+// function boooo(n) {
+//   for (i = 0; i < n.length; i++) {
+//     console.log('BOOOOO!');
+//   }
+// }
+
+// boooo([1, 2, 3, 4, 5]);
+
+//Array of hi:
+
+// function arrayOfHiNTimes(n) {
+//   let hiArray = [];
+//   for (let i = 0; i < n; i++) {
+//     hiArray[i] = 'hi';
+//   }
+//   return hiArray;
+// }
+
+// arrayOfHiNTimes(6); 
+
+// // BIG o(n)
+
+//Twitter Exercise For Sapce Complexity::
+
+//Find 1st , Find Nth...
