7f87005ac8407f68f3e476559340e086a3b58eaf
Zaki ul Hassan <staqiulhassan@gmail.com>
2026-08-29 18:49:09 +0500

    Update hash_table implementation with get method

--- Hash Tables/hashtable.js
+++ Hash Tables/hashtable.js
@@ -21,8 +21,23 @@ class HashTable {
     this.data[address].push([key, value]);
     return this.data;
   }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    console.log(currentBucket);
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
 console.log(myHashTable.set('grapes', 10000));
 console.log(myHashTable.set('apples', 54));
+console.log(myHashTable.get('grapes'));
